2b8530fc9ac57aec95083b40b57a44c0c52ba88c
Kurizu <83665497+crizmo@users.noreply.github.com>
2023-06-08 15:12:48 +0530

    🔥 Desk

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('path')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const RPC = require('discord-rpc');
+const rpc = new RPC.Client({
+    transport: 'ipc'
+})
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    autoHideMenuBar: true,
   })
 
   mainWindow.loadURL('https://emogit.vercel.app/')
-
 }
+
+rpc.on("ready", () => {
+  rpc.setActivity({
+      // details: "EmoGit-Desk",
+      state: "Commit with Emojis",
+      startTimestamp: new Date(),
+      largeImageKey: "emogit",
+      largeImageText: "Commit with Emojis",
+      buttons : [{label : "EmoGit" , url : "https://emogit.vercel.app/"},{label : "Discord" , url : "https://discord.gg/pyvUW9FAWm"}]
+  })
+
+  console.log("Successfully activated the RPC")
+})
+
+rpc.login({
+  clientId: process.env.CLIENT_ID
+})
 
 app.whenReady().then(() => {
   createWindow()
@@ -20,7 +41,7 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
